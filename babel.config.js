8c2720b7e34bd3b5d4113278d624750e4dc1ffc1
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-07-01 13:32:43 -0300

    feat: add babel and build script

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '~': './app/src/',
+        },
+      },
+    ],
+  ],
+  ignore: ['**/*.spec.ts'],
+};
